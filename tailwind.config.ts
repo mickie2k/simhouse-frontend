a17bc9a2283ea96af3432c92311d666e32ca25d9
mickie2k <tpookongmek@gmail.com>
2026-03-10 20:19:24 +0700

    fix: shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,29 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        navblack: "#242424",
-        primary1: "#FC6200",
-        primary2: "#FF9800",
-        primary1_hover: "#f05b01",
-        accent: "#FF8000",
-        black2: "#222222",
-        hrColor: "#C2C2C2",
-        secondText: "#717171",
-        borderColor1: "#E2E2E2",
-        borderColor2: "#D1D5DB"
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                navblack: "#242424",
+                primary1: "#FC6200",
+                primary2: "#FF9800",
+                primary1_hover: "#f05b01",
+                accent: "#FF8000",
+                black2: "#222222",
+                hrColor: "#C2C2C2",
+                secondText: "#717171",
+                borderColor1: "#E2E2E2",
+                borderColor2: "#D1D5DB",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
